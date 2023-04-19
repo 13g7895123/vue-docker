22639f75eee0c5dd1144c9360bd2ed61f21cf4c5
jarvisgit config --global user.name jarvis <13g7895123@gmail.com>
2023-04-19 04:18:43 +0000

    finished lesson 54

--- vue-app/src/router/index.js
+++ vue-app/src/router/index.js
@@ -1,7 +1,22 @@
 import { createRouter, createWebHistory } from "vue-router"
 import routes from "./routes"
 
-export default createRouter({
+const router = createRouter({
     routes,
     history: createWebHistory()
 })
+
+router.beforeEach((to, from) => {
+    // to and from are both route objects. must call `next`.
+    console.log('Global before each', to, from);
+    // if (to.path === '/tasks'){
+    //     return { 
+    //         name: 'login',
+    //         query: {
+    //             redirect: to.fullPath
+    //         }
+    //     }
+    // }
+})
+
+export default router
--- vue-app/src/router/routes.js
+++ vue-app/src/router/routes.js
@@ -1,12 +1,31 @@
+import HomePage from "../pages/HomePage.vue"
 import TaskPage from "../pages/TasksPage.vue"
 import LoginPage from "../pages/LoginPage.vue"
 import RegisterPage from "../pages/RegisterPage.vue"
+import SummaryPage from "../pages/SummaryPage.vue"
+import NotFoundErrorPage from "../pages/errors/NotFoundErrorPage.vue"
 
 const routes = [
     {
+        path: "/",
+        component: HomePage,
+        name: 'home',
+    },
+    {
         path: "/tasks",
         component: TaskPage,
-        name: 'tasks'
+        name: 'tasks',
+        // meta: {
+        //     auth: true
+        // }
+    },
+    {
+        path: "/summary",
+        component: SummaryPage,
+        name: 'summary',
+        // meta: {
+        //     auth: true
+        // }
     },
     {
         path: "/login",
@@ -17,6 +36,11 @@ const routes = [
         path: "/register",
         component: RegisterPage,
         name: 'register'
+    },
+    {
+        path: "/:notFound(.*)",
+        component: NotFoundErrorPage,
+        name: 'error.404'
     },
 ]
 
